518aabad822e02bf8b789d6fb18c93dd55347b09
Brindashree C B <brindashree@users.noreply.github.com>
2022-10-22 16:45:33 +0530

    feat: single lift movement logic

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const firstFloor = document.querySelector("[data-floor-num='1']");
 const lifts = document.querySelectorAll(".lift");
 const floors = document.querySelectorAll(".floor");
 
+const upBtn = document.querySelector(".btn-up");
+const downBtn = document.querySelector(".btn-down");
+
 const liftsArray = Array(lifts);
 const floorsArray = Array(floors);
 
@@ -14,12 +17,15 @@ const handleAddFloor = () => {
 		newFloor.classList.add("floor");
 		newFloor.innerHTML = `
                 <div class="btn-container">
-					<button class="btn btn-up">Up</button>
-					<button class="btn btn-down">Down</button>
+					<button class="btn btn-up" data-up-btn=${floorsArray.length + 1}>Up</button>
+					<button class="btn btn-down" data-down-btn=${
+						floorsArray.length + 1
+					}>Down</button>
 				</div>
     `;
 		floorContainerElement.prepend(newFloor);
 		floorsArray.push(newFloor);
+		newFloor.setAttribute("data-floor-num", floorsArray.length);
 	} else {
 		addFloorBtn.disabled = true;
 	}
@@ -37,5 +43,20 @@ const handleAddLift = () => {
 		addLiftBtn.disabled = true;
 	}
 };
+const handleLiftUp = (distance) => {
+	const transitionDistance =
+		parseInt(distance) == 1 ? 0 : (parseInt(distance) - 1) * 10;
+	lifts[0].style.bottom = `${transitionDistance}rem`;
+	lifts[0].style.transition = `2s linear`;
+};
+addEventListener("click", (e) => {
+	if (e.target.dataset.upBtn) {
+		handleLiftUp(e.target.dataset.upBtn);
+	} else if (e.target.dataset.downBtn) {
+		handleLiftUp(e.target.dataset.downBtn);
+	}
+});
+
 addFloorBtn.addEventListener("click", handleAddFloor);
 addLiftBtn.addEventListener("click", handleAddLift);
+upBtn.addEventListener("click", handleLiftUp);
